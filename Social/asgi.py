ff672de9078ac57fba6c8915f67d03dc0ae370e0
Jack <babuljackinfo@gmail.com>
2022-04-26 05:19:01 +0600

    asgi 2

--- Social/asgi.py
+++ Social/asgi.py
@@ -3,15 +3,17 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 from message.routing import ws_urls
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
+    'websocket':AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
         URLRouter(
             ws_urls
         )
     )
-
+)
 })
