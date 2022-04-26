397885509f22ec7d71e29f7af2f530f2f4478394
Jack <babuljackinfo@gmail.com>
2022-04-26 16:10:42 +0600

    debug update

--- Social/settings.py
+++ Social/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$k(414)&x4eui&2ru!@816q5)_-%4weck^r*iai2wh@(*39(b2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
