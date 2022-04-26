8833824fb86ca09ed348e7d7745e9e10b78ae117
Jack <babuljackinfo@gmail.com>
2022-04-26 11:59:42 +0600

    settings

--- Social/settings.py
+++ Social/settings.py
@@ -80,7 +80,6 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
-        'ROUTING': 'message.routing.ws_urls',
     },
 }
 
